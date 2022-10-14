5637f686af686c6d68cafee52cf96691d94fb6f7
Herdez <mherdez@users.noreply.github.com>
2022-10-14 09:53:40 -0500

    ejercicio terminado

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-const usuario1 = {
-  "id": 9,
-  "email": "tobias.funke@reqres.in",
-  "first_name": "Tobias",
-  "last_name": "Funke",
-  "avatar": "https://reqres.in/img/faces/9-image.jpg"
-  };
-  const usuario2 = {
-    "id": 10,
-    "email": "byron.fields@reqres.in",
-    "first_name": "Byron",
-    "last_name": "Fields",
-    "avatar": "https://reqres.in/img/faces/10-image.jpg"
-    }
-
 const listaDeUsuarios =  [
   {
   "id": 7,
@@ -55,24 +40,50 @@ const listaDeUsuarios =  [
   "first_name": "Rachel",
   "last_name": "Howell",
   "avatar": "https://reqres.in/img/faces/12-image.jpg"
-  }
-  ]
-
-// document.body.innerHTML = `
-//   <h1>Usuarios</h1>
-//   <div class="container">
-//     <div class="card">
-//       <img src="${usuario1.avatar}" alt="imagen">
-//       <h2>${usuario1.first_name} ${usuario1.last_name}</h2>
-//       <p>${usuario1.email}</p>
-//     </div>
-//     <div class="card">
-//       <img src="${usuario2.avatar}" alt="imagen">
-//       <h2>${usuario2.first_name} ${usuario2.last_name}</h2>
-//       <p>${usuario2.email}</p>
-//     </div>
-//   </div>
-// `;
+  },
+  {
+    "id": 1,
+    "email": "george.bluth@reqres.in",
+    "first_name": "George",
+    "last_name": "Bluth",
+    "avatar": "https://reqres.in/img/faces/1-image.jpg"
+    },
+    {
+    "id": 2,
+    "email": "janet.weaver@reqres.in",
+    "first_name": "Janet",
+    "last_name": "Weaver",
+    "avatar": "https://reqres.in/img/faces/2-image.jpg"
+    },
+    {
+    "id": 3,
+    "email": "emma.wong@reqres.in",
+    "first_name": "Emma",
+    "last_name": "Wong",
+    "avatar": "https://reqres.in/img/faces/3-image.jpg"
+    },
+    {
+    "id": 4,
+    "email": "eve.holt@reqres.in",
+    "first_name": "Eve",
+    "last_name": "Holt",
+    "avatar": "https://reqres.in/img/faces/4-image.jpg"
+    },
+    {
+    "id": 5,
+    "email": "charles.morris@reqres.in",
+    "first_name": "Charles",
+    "last_name": "Morris",
+    "avatar": "https://reqres.in/img/faces/5-image.jpg"
+    },
+    {
+    "id": 6,
+    "email": "tracey.ramos@reqres.in",
+    "first_name": "Tracey",
+    "last_name": "Ramos",
+    "avatar": "https://reqres.in/img/faces/6-image.jpg"
+    }
+  ];
 
 function createPage() {
   const h1 = document.createElement('h1');
@@ -112,8 +123,4 @@ function newCard( usuario ) {
 }
 
 createPage();
-newCard( listaDeUsuarios[0] );
-newCard( listaDeUsuarios[1] );
-newCard( listaDeUsuarios[2] );
-newCard( listaDeUsuarios[3] );
-newCard( listaDeUsuarios[4] );
+listaDeUsuarios.map( usuario => newCard(usuario) );
