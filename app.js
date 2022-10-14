8215e2504953c6535c775d711532e7d29931f47a
Herdez <mherdez@users.noreply.github.com>
2022-10-14 09:11:43 -0500

    tarjeta basica

---
+++ app.js
@@ -0,0 +1,7 @@
+const usuario = {
+    "id": 1,
+    "first_name": "Janet",
+    "last_name": "Weaver",
+    "email": "janet.weaver@reqres.in",
+    "avatar": "https://reqres.in/img/faces/2-image.jpg"
+  };
