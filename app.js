c87cff5327f6bec08e684f6a65a397a942dcf1b5
Herdez <mherdez@users.noreply.github.com>
2022-10-18 11:41:20 -0500

    usuarios ReqRes

--- app.js
+++ app.js
@@ -1,31 +1,75 @@
 // SELECCIONAR UN ELEMENTO DEL DOM
 const contenedor = document.querySelector('#container-cards');
+// FUNCION PARA CREAR UNA TARJETA
 
-function Card( imagen, titulo, texto ) {
+function Card( id, nombre, email, avatar ) {
   const cardBody = `<div class="card">
-    <img src=${imagen} alt="foto">
-    <h2>${titulo}</h2>
-    <p>${texto}</p>
+    <img src=${avatar} alt="${nombre}">
+    <h2>${nombre}</h2>
+    <p>${email}</p>
   </div>`;
   contenedor.innerHTML += cardBody;
 }
 
-Card(
-'https://picsum.photos/id/80/200',
-'Karem Diaz',
-'karem_diaz@hotmail.com'
-);
 
-Card(
-  'https://picsum.photos/id/90/200',
-  'Sebastian Pedroza',
-  'sebastian_pedroza@hotmail.com'
-);
+const usuarios = [
+  {
+    "id": 1,
+    "email": "george.bluth@reqres.in",
+    "first_name": "George",
+    "last_name": "Bluth",
+    "avatar": "https://reqres.in/img/faces/1-image.jpg"
+    },
+    {
+    "id": 2,
+    "email": "janet.weaver@reqres.in",
+    "first_name": "Janet",
+    "last_name": "Weaver",
+    "avatar": "https://reqres.in/img/faces/2-image.jpg"
+    },
+    {
+    "id": 3,
+    "email": "emma.wong@reqres.in",
+    "first_name": "Emma",
+    "last_name": "Wong",
+    "avatar": "https://reqres.in/img/faces/3-image.jpg"
+    },
+    {
+    "id": 4,
+    "email": "eve.holt@reqres.in",
+    "first_name": "Eve",
+    "last_name": "Holt",
+    "avatar": "https://reqres.in/img/faces/4-image.jpg"
+    },
+    {
+    "id": 5,
+    "email": "charles.morris@reqres.in",
+    "first_name": "Charles",
+    "last_name": "Morris",
+    "avatar": "https://reqres.in/img/faces/5-image.jpg"
+    },
+    {
+    "id": 6,
+    "email": "tracey.ramos@reqres.in",
+    "first_name": "Tracey",
+    "last_name": "Ramos",
+    "avatar": "https://reqres.in/img/faces/6-image.jpg"
+    }
+];
 
-Card(
-  'https://picsum.photos/id/100/200',
-  'Maria Elena',
-  'maria_elena@hotmail.com'
-);
+usuarios.map( usuario => Card(usuario.id, usuario.first_name + " " + usuario.last_name, usuario.email, usuario.avatar) )
 
+
+/* TIPOS DE CICLOS PARA RECORRER UN ARREGLO
+
+  - for
+  - for in
+  - for of
+  - forEach
+  - map
+*/
+
+
+// TODO: Crear Elementos HTML y
+//       utilizar ejemplo con usuarios
 
